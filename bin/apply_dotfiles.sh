6467ca656add739f1eb56fa544456ceb63286a2f
kevinjom <gokevinvan@gmail.com>
2016-06-19 13:42:20 +0800

    Change git user info with git hooks.

--- bin/apply_dotfiles.sh
+++ bin/apply_dotfiles.sh
@@ -3,19 +3,19 @@
 set -e
 
 WD=$(cd $(dirname $0) && pwd -P)
-DOT_FILE_DIR=$(dirname $WD)
+DOT_FILE_DIR=$(dirname $WD)/dots
 
-cd "$DOT_FILE_DIR/dots"
-
-if [ -f gituser ];then
-  cat gituser > gitconfig
-  cat gitconfig_shared >> gitconfig
-else
-  cat gitconfig_shared > gitconfig
-fi
-
+cd $DOT_FILE_DIR
 
 for file in $(find $(pwd) -maxdepth 1 -type f); do
   ln -sf $file ~/.$(basename $file)
 done
 
+dirs=$(find $(pwd) -mindepth 1 -maxdepth 1 -type d)
+
+for dir in $dirs; do
+  for file in $(find $dir -type f); do
+    linked_to=${file:${#DOT_FILE_DIR}}
+    ln -sf $file ~/$linked_to
+  done
+done
